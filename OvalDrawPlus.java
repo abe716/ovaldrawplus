7cb962f0075330be4b24902e82d7dac7801666e3
Ibrahim Mansour <abenansur@gmail.com>
2026-09-16 15:08:58 -0500

    updated code

--- OvalDrawPlus.java
+++ OvalDrawPlus.java
@@ -1,5 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class OvalDrawPlus {
-   public static void main(String[] args) {
-       System.out.println("Starting OvalDrawPlus...");
-   }
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting OvalDrawPlus...");
+
+        JFrame window = new JFrame("Oval Draw Plus");
+
+        window.setSize(400, 500);
+
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel text = new JLabel("Oval Draw Plus", JLabel.CENTER);
+
+        window.add(text);
+
+        window.setVisible(true);
+    }
 }
