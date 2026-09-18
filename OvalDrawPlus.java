21c80cae04dc4479ad62d5364fa66f03491e6305
Ibrahim Mansour <abenansur@gmail.com>
2026-09-18 18:30:31 -0500

    updated file

--- OvalDrawPlus.java
+++ OvalDrawPlus.java
@@ -2,14 +2,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class OvalDrawPlus extends JPanel {
+
+    
+    static final Color RECTANGLE_COLOR = new Color(0, 0, 139);
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.GREEN);
+        
+        g.setColor(RECTANGLE_COLOR);
+        g.fillRect(0, 0, getWidth(), getHeight());
 
+        // Draw the green oval
+        g.setColor(Color.GREEN);
         g.fillOval(0, 0, getWidth(), getHeight());
     }
 
@@ -21,7 +30,15 @@ public class OvalDrawPlus extends JPanel {
 
         window.setSize(400, 500);
 
+        window.setLocation(250, 150);
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.out.println("Closing OvalDrawPlus...");
+            }
+        });
 
         window.add(new OvalDrawPlus());
 
